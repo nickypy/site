b56dc26470fe8b444a616694d9ffe87bf1f34b2f
Nick Pagsanjan <pagsanjan.nick@gmail.com>
2024-03-31 17:46:45 -0700

    update styles

--- buko/blog.go
+++ buko/blog.go
@@ -64,7 +64,7 @@ func NewBlogRenderCache(prefix string, opts ...BlogOption) *BlogRenderCache {
 		goldmark.WithExtensions(
 			meta.Meta,
 			highlighting.NewHighlighting(
-				highlighting.WithStyle("dracula"),
+				highlighting.WithStyle("nord"),
 			),
 			extension.GFM,
 		),
